2070a91cabda7e7cb7c7ba6932404aa7e559caf4
Graeme Winter <graeme.winter@gmail.com>
2023-02-25 15:32:06 +0000

    Working time update

--- clock.py
+++ clock.py
@@ -2,20 +2,30 @@ import network
 import socket
 import time
 import struct
-
-from machine import Pin
-
-NTP_DELTA = 2208988800
-host = "pool.ntp.org"
-
-tz_url = "http://worldtimeapi.org/api/timezone/Europe/London"
-
-led = Pin("LED", Pin.OUT)
-
-from wifi import ssid, password
+import json
+import urequests
+import machine
 
 
-def set_time():
+def update_time():
+    connect()
+    offset = time_zone()
+    set_time(offset)
+
+
+def time_zone():
+    response = urequests.get("http://worldtimeapi.org/api/timezone/Europe/London")
+    offset = response.json()["utc_offset"]
+    response.close()
+    if offset == "+00:00":
+        return 0
+    # apply 3600s offset
+    return 3600
+
+
+def set_time(offset=0):
+    NTP_DELTA = 2208988800
+    host = "pool.ntp.org"
     NTP_QUERY = bytearray(48)
     NTP_QUERY[0] = 0x1B
     addr = socket.getaddrinfo(host, 123)[0][-1]
@@ -28,30 +38,26 @@ def set_time():
         s.close()
     val = struct.unpack("!I", msg[40:44])[0]
     t = val - NTP_DELTA
-    tm = time.gmtime(t)
-    machine.RTC().datetime((tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], 0))
+    tm = time.gmtime(t + offset)
+    machine.RTC().datetime((tm[0], tm[1], tm[2], tm[6], tm[3], tm[4], tm[5], 0))
 
 
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(ssid, password)
+def connect(max_wait=10):
+    from wifi import ssid, password
 
-max_wait = 10
-while max_wait > 0:
-    if wlan.status() < 0 or wlan.status() >= 3:
-        break
-    max_wait -= 1
-    print("waiting for connection...")
-    time.sleep(1)
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
 
-if wlan.status() != 3:
-    raise RuntimeError("network connection failed")
-else:
-    print("connected")
-    status = wlan.ifconfig()
-    print("ip = " + status[0])
+    while max_wait > 0:
+        if wlan.status() < 0 or wlan.status() >= 3:
+            break
+        max_wait -= 1
+        time.sleep(1)
 
-led.on()
-set_time()
-print(time.localtime())
-led.off()
+    if wlan.status() != 3:
+        raise RuntimeError("network connection failed")
+
+    return wlan.ifconfig()
+
+
